d9893c4ac4230f2c0bad827f5cda882a4483d744
Muhammad Sayed <muhammad.mahmoud98@eng-st.cu.edu.eg>
2021-01-19 13:19:41 +0200

    remove unnecessary import

--- src/features.py
+++ src/features.py
@@ -1,5 +1,4 @@
 #Global imports
-from skimage.feature.texture import greycomatrix
 from global_imports import np, pywt, cv2, feature, Image
 
 
@@ -50,7 +49,7 @@ def CSLBCoP(img, rad = 1, p=8):
     img_lbp = feature.local_binary_pattern(img, p, rad)
     img_lbp /= 8
     img_lbp = np.uint8(img_lbp)
-    glcm = greycomatrix(img_lbp, [1], [0, 45, 90, 135], 32)
+    glcm = feature.greycomatrix(img_lbp, [1], [0, 45, 90, 135], 32)
     return glcm.flatten()
 
 
